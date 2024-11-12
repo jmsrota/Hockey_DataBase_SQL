fcda9025791af562ddc2f9c4d342e6858310174a
jamesrota <jrota@ualberta.ca>
2024-11-12 15:16:14 -0700

    Added basic navbar, successfully connected database to website.

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,11 @@
-from flask import current_app as app
-from flask import render_template
-
+from flask import current_app as app, render_template, redirect, url_for
+from .models import db, Player, PlayerStats, GoalieStats, Team
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('BaseLayout.html')
+
+@app.route('/canucks)')
+def canucks():
+    player = Player.query.all()
+    return render_template('Canucks.html',player=player)
