b1a4ffd0af766ab88615de1d1fa6cdd37621814f
jamesrota <jrota@ualberta.ca>
2024-11-13 20:07:19 -0700

    Added Javascript file, Oilers route and played around with changing colors.

--- application/routes.py
+++ application/routes.py
@@ -3,9 +3,15 @@ from .models import db, Player, PlayerStats, GoalieStats, Team
 
 @app.route('/')
 def home():
-    return render_template('BaseLayout.html')
+    return render_template('BaseLayout.html', navbar_color="Default")
 
-@app.route('/canucks)')
-def canucks():
+@app.route('/Canucks)')
+def Canucks():
     player = Player.query.all()
-    return render_template('Canucks.html',player=player)
+    return render_template('Canucks.html', player=player, navbar_color='Canucks')
+
+@app.route('/Oilers')
+def Oilers():
+    player = Player.query.all()
+    return render_template('Oilers.html', player=player, navbar_color='Oilers')
+
